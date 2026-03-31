6999b5035b7b82a76dbab249a770fe07af92dd07
ityeri <ityeri9@gmail.com>
2026-03-31 16:18:34 +0900

    format: Overall code cleanup

--- examples/async.rs
+++ examples/async.rs
@@ -8,7 +8,8 @@ async fn main() {
 
     loop {
         println!("This message displays 2 times for a second!");
-        clock = clock.atick(2f64).await;
         println!("Time delta is: {}", clock.last_dt);
+        println!();
+        clock = clock.atick(2.0).await;
     }
 }
--- examples/sync.rs
+++ examples/sync.rs
@@ -7,7 +7,8 @@ fn main() {
 
     loop {
         println!("This message displays 2 times for a second!");
-        clock = clock.tick(2f64);
         println!("Time delta is: {}", clock.last_dt);
+        println!();
+        clock = clock.tick(2.0);
     }
 }
